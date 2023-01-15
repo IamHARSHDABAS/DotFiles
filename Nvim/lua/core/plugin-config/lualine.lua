ca3d206ed6e1333d8ce6f14aea7af69a7087bdb1
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-15 13:20:16 +0530

    Cleaned

--- Nvim/lua/core/plugin-config/lualine.lua
+++ Nvim/lua/core/plugin-config/lualine.lua
@@ -6,17 +6,17 @@ local lualine = require('lualine')
 -- Color table for highlights
 -- stylua: ignore
 local colors = {
-  bg       = '#202328',
-  fg       = '#bbc2cf',
-  yellow   = '#ECBE7B',
-  cyan     = '#008080',
-  darkblue = '#081633',
-  green    = '#98be65',
-  orange   = '#FF8800',
-  violet   = '#a9a1e1',
-  magenta  = '#c678dd',
-  blue     = '#51afef',
-  red      = '#ec5f67',
+  bg       = '#000000',
+  fg       = '#000000',
+  yellow   = '#FFAA00',
+  cyan     = '#00FFAA',
+  darkblue = '#00AAFF',
+  green    = '#AAFF00',
+  orange   = '#FF00AA',
+  violet   = '#AA00FF',
+  magenta  = '#AA00FF',
+  blue     = '#00AAFF',
+  red      = '#FF00AA',
 }
 
 local conditions = {
